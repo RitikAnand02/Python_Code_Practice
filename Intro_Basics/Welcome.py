6cec506861773dd1c9b2321d44be1eaa9bc80d13
RitikAnand02 <ritikanand.02062000@gmail.com>
2025-08-18 19:35:01 +0530

    new

---
+++ Intro_Basics/Welcome.py
@@ -0,0 +1,3 @@
+# Simple Python program to print a welcome message
+
+print("Welcome to Python!")
---
+++ Intro_Basics/add.py
@@ -0,0 +1,11 @@
+# Program to add two numbers
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Adding the numbers
+result = num1 + num2
+
+# Printing the result
+print("The sum is:", result)
